3dac2f4c481922fa5c8c64c09d4f69696cc52a3c
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-06 14:47:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var firstName=prompt("What is your first name?");
-var lastName=prompt("What is your last name?");
-var age=prompt("What is your age?");
-alert("Your name is "+ firstName + " " + lastName);
-alert("You have " + age + " years old");
-console.log("Your full name is "+ firstName + " " + lastName);
-console.log("You have " + age + " years old");
